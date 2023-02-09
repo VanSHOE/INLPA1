e025bcab3e51bc268dce9ea46fa3eb66582827f8
Rahul Garg <rahulgargofficial@gmail.com>
2023-02-09 23:38:47 +0530

    Made tokenisation into a function

--- main.py
+++ main.py
@@ -2,23 +2,31 @@ import re
 import numpy as np
 from pprint import pprint
 
-pridePrej = open("corpus/Pride and Prejudice - Jane Austen.txt", "r")
-# tokenize hashtags
-pridePrej = re.sub(r"#(\w+)", r"<HASHTAG> ", pridePrej.read())
-# tokenize mentions
-pridePrej = re.sub(r"@(\w+)", r"<MENTION> ", pridePrej)
-# tokenize urls
-pridePrej = re.sub(r"http\S+", r"<URL> ", pridePrej)
-# starting with www
-pridePrej = re.sub(r"www\S+", r"<URL> ", pridePrej)
 
-uniqueChar = set(pridePrej)
-specialChars = [' ', '*', '!', '?', '.', ',', ';', ':', '(', ')', '[', ']', '{', '}', '<', '>', '/', '\\', '|', '-',
-                '_', '=', '+', '`', '~', '@', '#', '$', '%', '^', '&', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+def get_token_list(path: str) -> list:
+    in_text = open(path, "r")
+    # tokenize hashtags
+    in_text = re.sub(r"#(\w+)", r"<HASHTAG> ", in_text.read())
+    # tokenize mentions
+    in_text = re.sub(r"@(\w+)", r"<MENTION> ", in_text)
+    # tokenize urls
+    in_text = re.sub(r"http\S+", r"<URL> ", in_text)
+    # starting with www
+    in_text = re.sub(r"www\S+", r"<URL> ", in_text)
 
-# pad the special characters with spaces
-for char in specialChars:
-    pridePrej = pridePrej.replace(char, ' ' + char + ' ')
+    special_chars = [' ', '*', '!', '?', '.', ',', ';', ':', '(', ')', '[', ']', '{', '}', '/', '\\', '|', '-',
+                     '_', '=', '+', '`', '~', '@', '#', '$', '%', '^', '&', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                     '9']
 
-pridePrejTokens = pridePrej.split()
-print(pridePrejTokens)
+    # pad the special characters with spaces
+    for char in special_chars:
+        in_text = in_text.replace(char, ' ')
+
+    # pad < and > with spaces
+    in_text = in_text.replace('<', ' <')
+    in_text = in_text.replace('>', '> ')
+
+    return in_text.split()
+
+
+print(get_token_list("corpus/Pride and Prejudice - Jane Austen.txt"))
